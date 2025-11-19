b0a5401a89dd68213b226d5a102a290b0d5dfd5f
Melji02 <ari.d.melji@gmail.com>
2025-11-19 20:21:41 +0100

    Modificacion para crear un conflicto con version local
    Test de conflictos

--- calculadora_2.java
+++ calculadora_2.java
@@ -1,3 +1,4 @@
 // Calculadora Tarea_2
 // Modificacion en local el 13/11/25 a las 10:50
 // Modificacion en remoto desde GitHub el 13/11/25
+// Modificacion en remoto para crear conflicto con version local
